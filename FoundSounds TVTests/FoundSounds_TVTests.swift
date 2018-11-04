963f8deb90cd1750c671c566b4cf3ba16514a438
David Jensenius <djensenius@gmail.com>
2018-11-04 15:18:15 -0500

    Get rid of swift errors

--- FoundSounds TVTests/FoundSounds_TVTests.swift
+++ FoundSounds TVTests/FoundSounds_TVTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import FoundSounds_TV
 
-class FoundSounds_TVTests: XCTestCase {
+class FoundSoundsTVTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- FoundSounds TVUITests/FoundSounds_TVUITests.swift
+++ FoundSounds TVUITests/FoundSounds_TVUITests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class FoundSounds_TVUITests: XCTestCase {
+class FoundSoundsTVUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
