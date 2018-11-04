34f30222fa49d22e05b3799a7db6336f3c4d1b1f
David Jensenius <djensenius@gmail.com>
2018-11-04 14:29:37 -0500

    Add all targets

---
+++ FoundSounds TV/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FoundSounds TV
+//
+//  Created by David Jensenius on 2018-11-04.
+//  Copyright © 2018 David Jensenius. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FoundSounds TV/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FoundSounds TV
+//
+//  Created by David Jensenius on 2018-11-04.
+//  Copyright © 2018 David Jensenius. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
